93b629621cff585874587231f26e16a8cac83d1f
Rubab Tariq <rubab.khawaja786@gmail.com>
2018-08-13 13:48:23 +0500

    Initial commit

---
+++ IdentityManagementWebService/AddIdentity.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IdentityManagementWebService
+    {
+    public partial class AddIdentity : System.Web.UI.Page
+        {
+        protected void Page_Load (object sender, EventArgs e)
+            {
+            countDiv.InnerText = "2";
+
+            }
+        }
+    }
